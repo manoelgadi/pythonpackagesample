95dd0bcc206f71e74e9309688ebd3f88031e998c
manoelgadi <manoelgadi@gmail.com>
2018-06-07 22:11:35 +0200

    3rd commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pythonpackagesample',
       version='0.1',  # Development release
       description='Python package for Bravo Capital!',
-      url='xxx',
+      url='https://github.com/manoelgadi/pythonpackagesample',
       author='Manoel Gadi',
       author_email='manoel.gadi@bravocapital.es',
       license='MIT',
