ded1a5c4660a4cfc152050ff6f24181dc7da0533
manoelgadi <manoelgadi@gmail.com>
2018-06-07 22:51:56 +0200

    student sample 1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='pythonpackagesample',
       version='0.1',  # Development release
-      description='Python package for Bravo Capital!',
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
       url='https://github.com/manoelgadi/pythonpackagesample',
-      author='Manoel Gadi',
-      author_email='manoel.gadi@bravocapital.es',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
       license='MIT',
       packages=['pythonpackagesample'],
       zip_safe=False)
